8a99d57bb6eb6a5aa2e571cf4efad5a38a38f4f6
Safwan <92028514+sfwnisme@users.noreply.github.com>
2023-11-30 17:27:04 +0300

    create phoneNumber variable

--- whatsapp icon/main.js
+++ whatsapp icon/main.js
@@ -1,6 +1,7 @@
 let link = document.createElement("div");
 link.className = "wapp-icon";
-link.innerHTML = `<a href='https://api.whatsapp.com/send/?phone=966504706185&text&type=phone_number&app_absent=0'>
+let phoneNumber = '966595480826'
+link.innerHTML = `<a href='https://wa.me/${phoneNumber}'>
 <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-whatsapp" width="40" height="40" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
 <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
 <path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9"></path>
